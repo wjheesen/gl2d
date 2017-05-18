89f67bcea0a7a1d6b633f6b9c0f0af7cb6ff4d18
William Heesen <vindicayte@gmail.com>
2017-05-18 15:01:24 -0400

    Rename shape class to drawable

--- src/drawable/drawable.ts
+++ src/drawable/drawable.ts
@@ -7,29 +7,29 @@ import { IMat2d, Mat2dStruct, ScaleToFit, Mat2d } from '../struct/mat2d';
 import { Mesh } from "./mesh";
 
 /**
- * 2D shape determined by matrix transformation of a mesh.
+ * 2D drawable determined by matrix transformation of a mesh.
  */
-export abstract class Shape<R extends Renderer> {
+export abstract class Drawable<R extends Renderer> {
 
     /**
-     * Contains the vertex and index data for this shape.
+     * Contains the vertex and index data for this drawable.
      */
     mesh: Mesh;
 
     /**
-     * The color of this shape (if any).
+     * The color of this drawable (if any).
      */
     color?: ColorFStruct;
 
     /**
-     * The matrix that maps this shape from model space to world space.
+     * The matrix that maps this drawable from model space to world space.
      */
     matrix: Mat2dStruct;
 
     /**
-     * Creates a shape with the specified matrix transformation.
-     * @param mesh the static mesh data for this shape.
-     * @param matrix matrix transformation of the shape. Defaults to identity.
+     * Creates a drawable with the specified matrix transformation.
+     * @param mesh the static mesh data for this drawable.
+     * @param matrix matrix transformation of the drawable. Defaults to identity.
      */
     constructor(mesh: Mesh, color?: ColorFStruct, matrix?: Mat2dStruct) {
         this.mesh = mesh;
@@ -38,23 +38,23 @@ export abstract class Shape<R extends Renderer> {
     }
 
     /**
-     * Measures the boundaries of this shape.
-     * @returns the boundaries of this shape, or null if the shape has no vertices.
+     * Measures the boundaries of this drawable.
+     * @returns the boundaries of this drawable, or null if the drawable has no vertices.
      */
     measureBoundaries() {
         let bounds = <Rect> null;
         let meshVertices = this.mesh.vertices;
-        // Map each mesh vertex to get the shape vertex that must be enclosed
+        // Map each mesh vertex to get the drawable vertex that must be enclosed
         if(meshVertices.moveToFirst()){
-            // Enclose first shape vertex
+            // Enclose first drawable vertex
             let matrix = this.matrix;
-            let shapeVertex = new Point();
-            matrix.map(meshVertices, shapeVertex);
-            bounds = Rect.unionOfPoints([shapeVertex]);
+            let drawableVertex = new Point();
+            matrix.map(meshVertices, drawableVertex);
+            bounds = Rect.unionOfPoints([drawableVertex]);
             // Enclose remaining vertices
             while(meshVertices.moveToNext()){
-                 matrix.map(meshVertices, shapeVertex);
-                 bounds.unionPoint(shapeVertex);
+                 matrix.map(meshVertices, drawableVertex);
+                 bounds.unionPoint(drawableVertex);
             }
         }
         // Bounds will be null if model has no vertices
@@ -62,9 +62,9 @@ export abstract class Shape<R extends Renderer> {
     }
 
     /**
-     * Converts a point in world space to a point in this shape's model space using the inverse model matrix.
+     * Converts a point in world space to a point in this drawable's model space using the inverse model matrix.
      * @param pointInWorld the point in world space.
-     * @param inverse the inverse of this shape's model matrix. If undefined, the inverse matrix will be calculated on the fly.
+     * @param inverse the inverse of this drawable's model matrix. If undefined, the inverse matrix will be calculated on the fly.
      * @param dst where to write the result. Defaults to new point.
      * @returns dst.
      */
@@ -76,7 +76,7 @@ export abstract class Shape<R extends Renderer> {
 
     /**
      * Converts the vertex at the specified position from model space to world space.
-     * @param position the position of the vertex in the vertex buffer associated with this shape's mesh.
+     * @param position the position of the vertex in the vertex buffer associated with this drawable's mesh.
      * @returns the position of the vertex in world space, or null if no vertex exists at the specified position.
      */
     convertVertexToWorldSpace(position: number){
@@ -90,32 +90,32 @@ export abstract class Shape<R extends Renderer> {
     }
 
     /**
-     * Checks if this shape contains the point (x,y).
+     * Checks if this drawable contains the point (x,y).
      * @param point the point to check.
-     * @param inverse the inverse of this shape's model matrix. If undefined, the inverse matrix will be calculated on the fly.
+     * @param inverse the inverse of this drawable's model matrix. If undefined, the inverse matrix will be calculated on the fly.
      */
     contains(pt: IPoint, inverse?: IMat2d) {
         let modelPoint = this.convertPointToModelSpace(pt, inverse);
-        // this shape contains the point if its mesh contains the model point
+        // this drawable contains the point if its mesh contains the model point
         return this.mesh.bounds.containsPoint(modelPoint) && this.mesh.vertices.contains(modelPoint);
     }
 
     /**
-     * Offsets this shape by the specified vector.
+     * Offsets this drawable by the specified vector.
      */
     offset(vec: IVec2) {
         this.offset$(vec.x, vec.y);
     }
 
     /**
-     * Offsets this shape by the vector (dx,dy).
+     * Offsets this drawable by the vector (dx,dy).
      */
     offset$(dx: number, dy: number) {
         this.matrix.postTranslate$(dx, dy);
     }
 
     /**
-     * Transforms this shape by the specified matrix.
+     * Transforms this drawable by the specified matrix.
      * @param matrix the transformation matrix.
      */
     transform(matrix: IMat2d) {
@@ -123,7 +123,7 @@ export abstract class Shape<R extends Renderer> {
     }
 
     /**
-     * Maps this shape to the destination Rect using the specified scale to fit option.
+     * Maps this drawable to the destination Rect using the specified scale to fit option.
      * @param dst the destination rectangle.
      * @param stf the scale to fit option.
      */
@@ -132,13 +132,13 @@ export abstract class Shape<R extends Renderer> {
     }
 
     /**
-     * Stretch-rotates this shape across the line from p1 to p2.
+     * Stretch-rotates this drawable across the line from p1 to p2.
      */
     stretchAcrossLine(p1: IPoint, p2: IPoint) {
         let bounds = this.mesh.bounds;
         let meshPivot = bounds.centerTop();
         let meshControl = bounds.centerBottom();
-        // *Translate from mesh pivot to shape pivot
+        // *Translate from mesh pivot to drawable pivot
         let pivot = p1;
         let vec = Vec2.fromPointToPoint(meshPivot, pivot);
         let start = Vec2.create(meshControl); start.add(vec);
@@ -149,7 +149,7 @@ export abstract class Shape<R extends Renderer> {
     }
 
     /**
-     * Draws this shape using the specified renderer.
+     * Draws this drawable using the specified renderer.
      */
     abstract draw: (renderer: R) => void;
 }
